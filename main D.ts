b6469607681ff0806ea661be26a60d44fbdd0c7c
mshalaby007 <30973567+mshalaby007@users.noreply.github.com>
2022-10-22 04:20:35 -0400

    interfaces

---
+++ main D.ts
@@ -0,0 +1,24 @@
+//interfaces
+//object literal 
+//point: { x: number, y: number }
+
+// //inline annotation 
+// let drawPoint = (point: { x: number, y: number }) => {
+//     // ...
+// }
+
+
+interface Point {
+    x: number,
+    y: number
+}
+
+//inline annotation 
+let drawPoint = (point: Point) => {
+    // ...
+}
+
+// drawPoint({
+//     x: 1,
+//     y: 2
+// })
