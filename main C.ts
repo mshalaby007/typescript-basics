e48aa2223327604d4a0dd1a0027a9a58ac2b32f8
mshalaby007 <30973567+mshalaby007@users.noreply.github.com>
2022-10-22 04:10:29 -0400

    Arrow function examples

---
+++ main C.ts
@@ -0,0 +1,14 @@
+let log = function (message) {
+    console.log(message)
+}
+///////////////////////////////////////////
+//Arrow function examples below
+let doLog1 = (message) => {
+    console.log(message)
+}
+///////////////////////////////////////////
+let doLog2 = (message) => console.log(message)
+///////////////////////////////////////////
+let doLog3 = message => console.log(message)
+///////////////////////////////////////////
+let doLog4 = () => console.log()
