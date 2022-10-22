3fc65cf7b7062fb4dc46edd3be1024d793698fd2
mshalaby007 <30973567+mshalaby007@users.noreply.github.com>
2022-10-22 04:49:28 -0400

    classes contin / object creation

---
+++ main F.ts
@@ -0,0 +1,21 @@
+class Point {
+    x: number;
+    y: number;
+
+    draw() {
+        console.log('X: ' + this.x + ' ,Y: ' + this.y);
+    }
+
+    // getDistance(another: Point) {
+    //     console.log()
+    // }
+}
+
+//object
+let point = new Point();
+
+point.x = 1
+point.y = 2
+point.draw();
+
+
